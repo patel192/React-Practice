ccb970a4f7b700a59a9d376f13cbba8fc6a2fa3d
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-06-08 22:24:54 +0530

    conditional rendering example

--- src/Components/ConditionalRendering.jsx
+++ src/Components/ConditionalRendering.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export const ConditionalRendering = () => {
+  const array = [1, 2, 3, 4, 5, 6];
+  const buttons = array.map((item) => {
+    return <button>{item}</button>;
+  });
+  const slicedbuttons = array.slice(0, 3).map((Element) => {
+    return <button>{Element}</button>;
+  });
+
   return (
-    <div>ConditionalRendering</div>
-  )
-}
+    <div>
+      <div>{buttons}</div>
+
+      <div>
+        (if ({array.length === 6}) {{ slicedbuttons }})
+      </div>
+    </div>
+  );
+};
