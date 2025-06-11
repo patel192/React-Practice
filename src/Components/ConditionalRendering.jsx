6e250e038565f51ac60b0d5b1b9f91d42b234e48
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-06-11 12:43:06 +0530

    conditional rendering

--- src/Components/ConditionalRendering.jsx
+++ src/Components/ConditionalRendering.jsx
@@ -2,14 +2,23 @@ import React from "react";
 
 export const ConditionalRendering = () => {
   const array = [1, 2, 3, 4, 5, 6];
+  const array2 = [6, 5, 4, 3, 2, 1];
   const buttons = array.map((item) => {
     return <button>{item}</button>;
   });
   let slicedbuttons;
-  slicedbuttons = array.length === 7 ? array.map((item)=>{
-    return <button>{item}</button>
-  }) : [<button>NA</button>]
-return (
+  slicedbuttons =
+    array.length && array2.length != 0
+      ? array.map((item) => {
+          return (
+            <button>{item}</button> &&
+            array2.map((item2) => {
+            return <button>{item2}</button>;
+            })
+          );
+        })
+      : [<h1>Not Applicable</h1>]
+  return (
     <div>
       <div>{buttons}</div>
       <div>{slicedbuttons}</div>
