5f61b574e3eac8ac609f89451907a2356c8cd46e
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-06-12 11:16:39 +0530

    usestate counter

--- src/Components/ConditionalRendering.jsx
+++ src/Components/ConditionalRendering.jsx
@@ -1,20 +1,11 @@
 import React from "react";
-
-export const ConditionalRendering = () => {
-  const array = [1, 2, 3, 4, 5, 6];
-  const array2 = [6, 5, 4, 3, 2, 1];
-  const buttons = array.map((item) => {
-    return <button>{item}</button>;
-  });
-  let slicedbuttons;
-  slicedbuttons = array.length === array2.length && array.map((item)=>{
-    return <button>{item}</button>
-  })
-    
+import { useState } from "react";
+export const ConditionalRendering = () => {   
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <div>{buttons}</div>
-      <div>{slicedbuttons}</div>
+      <div>{count}</div>
+      <div><button onClick={()=>setCount(count+1) }>ClickMe</button></div>
     </div>
   );
 };
