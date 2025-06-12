2c1a5ba4941c8630acf0f5d40556937813cc0774
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-06-12 12:08:52 +0530

    count the number of charaters using the useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import { UseState } from "./Components/UseState";
 import { UseState2 } from "./Components/UseState2";
 import { UseState3 } from "./Components/UseState3";
+import { UseState4 } from "./Components/UseState4";
 
 function App() {
   return (
     <>
       <UseState />
       <UseState2 />
-      <UseState3/>
+      <UseState3 />
+      <UseState4 />
     </>
   );
 }
---
+++ src/Components/UseState4.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+export const UseState4 = () => {
+    const [textlength, settextlength] = useState()
+  return (
+    <div>
+        <h1>{textlength}</h1>
+        <input type="text" onChange={(event)=>{settextlength(event.target.value.length)}} />
+    </div>
+  )
+}
