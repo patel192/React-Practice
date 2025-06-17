420136a293b0c325265b12b7d0e34ffb1117e183
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-06-17 21:28:41 +0530

    useCallback component

--- src/Components/useCallback/UseCallback1.jsx
+++ src/Components/useCallback/UseCallback1.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import { useState } from 'react'
+import { ChildComponent } from './ChildComponent'
 export const UseCallback1 = () => {
+    const [count, setcount] = useState(0)
+    const handleclick=()=>{
+        alert("button clicked")
+    }
   return (
-    <div>UseCallback1</div>
+    <div>
+        <p>{count}</p>
+        <ChildComponent onClick={handleclick}/>
+        <button onClick={(count)=>setcount(count+1)}>Increment</button>
+    </div>
   )
 }
